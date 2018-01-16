7bc59cbf824cc0b58c1b385e2bfa7f74794b739c
Anwesh43 <anweshthecool0@gmail.com>
2018-01-16 08:12:19 +0530

    adding TextColoredView in Activity

--- app/src/main/java/ui/anwesome/com/kotlintextcoloredview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlintextcoloredview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlintextcoloredview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.textcoloredview.TextColoredView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TextColoredView.create(this,"Hello brother")
     }
 }
