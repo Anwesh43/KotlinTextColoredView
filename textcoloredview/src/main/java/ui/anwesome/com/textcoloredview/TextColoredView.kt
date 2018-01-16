aaae0c40dadeebb4230e910ab525e15b2ad5cd65
Anwesh43 <anweshthecool0@gmail.com>
2018-01-16 07:31:42 +0530

    adding TextColoredView

--- textcoloredview/src/main/java/ui/anwesome/com/textcoloredview/TextColoredView.kt
+++ textcoloredview/src/main/java/ui/anwesome/com/textcoloredview/TextColoredView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.textcoloredview
 /**
  * Created by anweshmishra on 16/01/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class TextColoredView(ctx:Context,var color:Int = Color.RED):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
