f56b9954f2b4afdac8c5e43c8ef2f2f01d1dbdb8
Anwesh43 <anweshthecool0@gmail.com>
2018-01-16 07:55:31 +0530

    adding Renderer in View

--- textcoloredview/src/main/java/ui/anwesome/com/textcoloredview/TextColoredView.kt
+++ textcoloredview/src/main/java/ui/anwesome/com/textcoloredview/TextColoredView.kt
@@ -8,13 +8,14 @@ import android.content.*
 import android.graphics.*
 class TextColoredView(ctx:Context,var text:String,var color:Int = Color.RED):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = Renderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
