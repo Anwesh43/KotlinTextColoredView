98c03ee341f380f50621dcb03634d3cb8b8d1e75
Anwesh43 <anweshthecool0@gmail.com>
2018-01-16 07:42:05 +0530

    adding ColoredTextState

--- textcoloredview/src/main/java/ui/anwesome/com/textcoloredview/TextColoredView.kt
+++ textcoloredview/src/main/java/ui/anwesome/com/textcoloredview/TextColoredView.kt
@@ -39,4 +39,19 @@ class TextColoredView(ctx:Context,var text:String,var color:Int = Color.RED):Vie
 
         }
     }
+    data class ColoredTextState(var scale:Float = 0f,var dir:Float = 0f,var prevScale:Float = 0f) {
+        fun update(stopcb:(Float)->Unit) {
+            scale += 0.1f*dir
+            if(Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                stopcb(scale)
+            }
+        }
+        fun startUpdating(startcb:()->Unit) {
+            dir = 1-2*scale
+            startcb()
+        }
+    }
 }
