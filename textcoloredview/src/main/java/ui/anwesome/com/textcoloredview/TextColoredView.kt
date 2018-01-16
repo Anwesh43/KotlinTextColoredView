ae4cb46a0606e50e72b2d9f87cb69590d6680e07
Anwesh43 <anweshthecool0@gmail.com>
2018-01-16 07:39:52 +0530

    adding ColoredText

--- textcoloredview/src/main/java/ui/anwesome/com/textcoloredview/TextColoredView.kt
+++ textcoloredview/src/main/java/ui/anwesome/com/textcoloredview/TextColoredView.kt
@@ -6,7 +6,7 @@ package ui.anwesome.com.textcoloredview
 import android.view.*
 import android.content.*
 import android.graphics.*
-class TextColoredView(ctx:Context,var color:Int = Color.RED):View(ctx) {
+class TextColoredView(ctx:Context,var text:String,var color:Int = Color.RED):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
 
@@ -18,5 +18,25 @@ class TextColoredView(ctx:Context,var color:Int = Color.RED):View(ctx) {
             }
         }
         return true
+    }
+    data class ColoredText(var text:String,var color:Int,var x:Float,var y:Float,var size:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.textSize = size
+            val tw = paint.measureText(text)
+            canvas.save()
+            canvas.translate(x,y)
+            val path = Path()
+            path.addRect(RectF(-tw/2,-size/2,tw/2,size/2),Path.Direction.CW)
+            canvas.clipPath(path)
+            paint.color = color
+            canvas.drawText(text,-tw/2,0f,paint)
+            canvas.restore()
+        }
+        fun update(stopcb:(Float)->Unit) {
+
+        }
+        fun startUpdating(startcb:()->Unit) {
+
+        }
     }
 }
