b35b43c9027fe12f897c0df3d944fc15c32bdcde
Anwesh43 <anweshthecool0@gmail.com>
2018-01-16 08:31:27 +0530

    adding OnTextColoredListener in Activity

--- app/src/main/java/ui/anwesome/com/kotlintextcoloredview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlintextcoloredview/MainActivity.kt
@@ -2,12 +2,18 @@ package ui.anwesome.com.kotlintextcoloredview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.textcoloredview.TextColoredView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TextColoredView.create(this,"Hello brother")
+        val view = TextColoredView.create(this,"Hello brother")
+        view.addOnTextColoredListener({
+            Toast.makeText(this,"Text Colored",Toast.LENGTH_SHORT).show()
+        },{
+            Toast.makeText(this,"Text Colored Over",Toast.LENGTH_SHORT).show()
+        })
     }
 }
